29b603450884c709920599fda696f98ad22975b0
Maria Fernanda Morais do Amaral <mariafernandamamaral@gmail.com>
2025-12-11 00:23:29 -0300

    qry

---
+++ src/lib/qry_handler/qry_handler.h
@@ -0,0 +1,17 @@
+#ifndef QRY_HANDLER_H
+#define QRY_HANDLER_H
+
+#include "../geo_handler/geo_handler.h"
+
+/**
+ * Processa um arquivo de consultas (.qry) e executa operações sobre as formas geométricas.
+ * Gera arquivos de saída (texto e SVG) com os resultados.
+ *
+ * @param cidade A estrutura Geo com todas as formas cadastradas
+ * @param qryPath Caminho do arquivo de consultas (.qry)
+ * @param outPath Caminho base para arquivos de saída (sem extensão)
+ * @param geoName Nome do arquivo .geo (para referência em logs)
+ */
+void qry_processar(Geo cidade, const char *qryPath, const char *outPath, const char *geoName);
+
+#endif // QRY_HANDLER_H
